36ab2caea21ded65f0ca643c36ca96c87be5b99a
Cédric <189109074+ItsMeCedric@users.noreply.github.com>
2025-01-29 22:57:04 +0100

    add GDPR & Legals page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router";
 import SignIn from "./pages/SignIn/SignIn";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import RGPD from "./pages/RGPD/RGPD";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route index path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signin" element={<SignIn />} />
+      <Route path="/RGPD" element={<RGPD />} />
     </Routes>
   );
 };
