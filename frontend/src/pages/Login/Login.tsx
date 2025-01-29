ad5603eb65739224455481222463ae8da3844a45
louis-stintzy <louis.stintzy@gmail.com>
2025-01-29 16:26:26 +0000

    feat(auth-forms): refactor login form with react-hook-form

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,18 +1,28 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import styles from "./Login.module.css";
 
+type LoginFormInput = {
+  email: string;
+  password: string;
+};
+
 const Login = () => {
+  const { register, handleSubmit } = useForm<LoginFormInput>();
+  const onSubmit: SubmitHandler<LoginFormInput> = (data) => console.log(data);
+
   return (
     <div className={styles.auth_container}>
       <div className={styles.form_wrapper}>
         <h2>Connexion</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.form_group}>
             <label htmlFor={styles.login_email}>Email</label>
-            <input type="email" id="login-email" placeholder="Entrez votre email" required />
+            <input {...register("email", { required: true })} placeholder="Entrez votre email" />
           </div>
           <div className={styles.form_group}>
             <label htmlFor="login-password">Mot de passe</label>
-            <input type="password" id="login-password" placeholder="Entrez votre mot de passe" required />
+            <input {...register("password", { required: true })} placeholder="Entrez votre mot de passe" />
           </div>
           <button type="submit" className={styles.btn}>
             Se connecter
