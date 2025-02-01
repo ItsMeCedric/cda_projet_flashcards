4834ea03865c1d8e670c70d8863122af2b397554
louis-stintzy <louis.stintzy@gmail.com>
2025-02-01 21:33:24 +0100

    refactor(auth-forms): replace container divs with reusable FormContainer in Login and SignIn

--- frontend/src/components/Common/Form/FormContainer/FormContainer.tsx
+++ frontend/src/components/Common/Form/FormContainer/FormContainer.tsx
@@ -2,12 +2,18 @@ import styles from './FormContainer.module.css';
 
 interface FormContainerProps {
   children: React.ReactNode;
+  classNames?: {
+    container?: string;
+    formWrapper?: string;
+  };
 }
 
-function FormContainer({ children }: FormContainerProps) {
+function FormContainer({ children, classNames }: FormContainerProps) {
   return (
-    <div className={styles.auth_container}>
-      <div className={styles.form_wrapper}>{children}</div>
+    <div className={`${styles.auth_container} ${classNames?.container}`}>
+      <div className={`${styles.form_wrapper} ${classNames?.formWrapper}`}>
+        {children}
+      </div>
     </div>
   );
 }
