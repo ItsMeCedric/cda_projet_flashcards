76c48b83fcbc62f1ca6b31616c86cd97e506c682
Laurent <seres@seres.eu.org>
2025-02-12 16:26:55 +0100

    removed debug endpoint

--- backend/src/routers/authRouter.ts
+++ backend/src/routers/authRouter.ts
@@ -4,8 +4,5 @@ import authController from "../controllers/authController";
 const router = Router();
 
 router.post("/register", authController.register);
-router.get("/register", (req, res) => {
-  res.send("working!");
-});
 
 export default router;
