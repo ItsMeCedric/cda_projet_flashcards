7f459eca952b1ec5ddbeacc1ee0dd88da7cbdfac
Nicolas Vanlauwe <nicolas.vanlauwe@hotmail.com>
2025-02-19 11:35:06 +0100

    ajout logique du register et certaines fonctions dans le userRepository needed

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,6 +1,8 @@
 import userRepository from "../repositories/userRepository";
+import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 
-interface RegisterData {
+export interface RegisterData {
   username: string;
   email: string;
   password: string;
@@ -11,8 +13,23 @@ interface LoginData {
   password: string;
 }
 
-const register = (data: RegisterData) => {
-  return userRepository.findAll();
+const register = async (data: RegisterData) => {
+  const existingEmail = await userRepository.findByEmail(data.email);
+  const existingUsername = await userRepository.findByUsername(data.username);
+  if (existingEmail || existingUsername) {
+    throw new Error("Vous avez déjà un compte");
+  }
+
+  const hash = await argon2.hash(data.password, {
+    type: argon2.argon2id,
+  });
+  const newData = {
+    username: data.username,
+    email: data.email,
+    hash: hash,
+  };
+  const newUser = await userRepository.addUser(newData);
+  // return userRepository.findAll();
   //TODO: do register logic
   // hash password using argon2
   // create new user with `data`
