d07958b08f9f61e79ea0c235cd7044352553ba0f
Nicolas Vanlauwe <nicolas.vanlauwe@hotmail.com>
2025-02-19 11:35:06 +0100

    ajout logique du register et certaines fonctions dans le userRepository needed

--- backend/src/repositories/userRepository.ts
+++ backend/src/repositories/userRepository.ts
@@ -1,7 +1,16 @@
 import User from "../../models/user";
+import { UserCreationAttributes } from "../../models/user";
 
+const addUser = async (data: UserCreationAttributes) => {
+  return await User.create(data);
+};
 const findAll = async () => {
   return await User.findAll();
 };
-
-export default { findAll };
+const findByEmail = async (data: string) => {
+  return await User.findOne({ where: { email: data } });
+};
+const findByUsername = async (data: string) => {
+  return await User.findOne({ where: { username: data } });
+};
+export default { findAll, findByEmail, findByUsername, addUser };
