6d83c881cb970dce07e851a0f6fd3287a235222b
Laurent <seres@seres.eu.org>
2025-02-24 10:30:31 +0100

    wtf

--- backend/src/routers/deckRouter.ts
+++ backend/src/routers/deckRouter.ts
@@ -1,33 +1,12 @@
-import { Request, Response } from "express";
-import deckService from "../services/deckService";
+import { Router } from "express";
+import deckController from "../controllers/deckController";
 
-const getAllDecks = async (req: Request, res: Response) => {
-  const decks = await deckService.getAllDecks();
-  res.status(200).json(decks);
-};
+const router = Router({ mergeParams: true });
 
-const findById = async (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
-  const deck = await deckService.findById(id);
-  res.status(200).json(deck);
-};
+router.get("/", deckController.getAllDecks);
+router.get("/:deckId", deckController.findById);
+router.post("/", deckController.create);
+router.patch("/:deckId", deckController.update);
+router.delete("/:deckId", deckController.destroy);
 
-const create = async (req: Request, res: Response) => {
-  const data = req.body;
-  const deck = await deckService.create(data);
-  res.status(201).json(deck);
-};
-
-const update = async (req: Request, res: Response) => {
-  const data = req.body;
-  const deck = await deckService.update(data);
-  res.status(200).json(deck);
-};
-
-const destroy = async (req: Request, res: Response) => {
-  const id = parseInt(req.body.id);
-  await deckService.destroy(id);
-  res.status(200).json({ id: id });
-};
-
-export default { getAllDecks, findById, create, update, destroy };
+export default router;
