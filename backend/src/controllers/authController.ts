e7e391a60371102f6293b760e8a07bd55e3661af
Nicolas Vanlauwe <nicolas.vanlauwe@hotmail.com>
2025-02-25 16:16:39 +0100

    ajout du middleware et fix du login et register

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -3,15 +3,20 @@ import authService from "../services/authService";
 
 const register = (req: Request, res: Response) => {
   const data = req.body;
-  const user = authService.register(data);
-  res.status(201).json(user);
+  authService.register(data);
+  res.status(201).json({ message: "Compte créé avec succés" });
 };
 
 const login = (req: Request, res: Response) => {
   const data = req.body;
   const response = authService.login(data);
-  res.setHeader("Set-Cookie", `token=${response}; HttpOnly;`);
+  res.cookie("Authorization", "Bearer ${token}", { secure: true, httpOnly: true, sameSite: "strict" });
   res.status(200).json(response);
 };
 
-export default { register, login };
+const logout = (req: Request, res: Response) => {
+  res.clearCookie("Authorization");
+  res.sendStatus(200);
+};
+
+export default { register, login, logout };
