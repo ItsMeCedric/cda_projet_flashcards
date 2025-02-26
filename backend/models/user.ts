ea77108de56abef7444208e70fc413e18ef5f294
Laurent <seres@seres.eu.org>
2025-02-26 10:43:06 +0100

    fix models

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,6 +1,15 @@
 "use strict";
 
-import { Model, InferCreationAttributes, CreationOptional, InferAttributes } from "sequelize";
+import {
+  Model,
+  InferCreationAttributes,
+  CreationOptional,
+  InferAttributes,
+  Association,
+  Sequelize,
+  DataTypes,
+} from "sequelize";
+import Deck from "./deck";
 
 class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
   declare id: CreationOptional<string>;
@@ -8,6 +17,42 @@ class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
   declare email: string;
   declare profilePicture: CreationOptional<string>;
   declare hash: string;
+
+  declare static associations: {
+    decks: Association<User, Deck>;
+  };
+
+  static initialize(sequelize: Sequelize) {
+    User.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        profilePicture: {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        hash: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "User",
+      }
+    );
+  }
 }
 
 export default User;
