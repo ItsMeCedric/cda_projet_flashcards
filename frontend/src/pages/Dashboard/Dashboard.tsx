ae7a1b94efa55ae0c32c71494c43afdc7f4874a9
Cédric <189109074+ItsMeCedric@users.noreply.github.com>
2025-03-03 00:58:45 +0100

    fix1

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,17 @@
-import styles from "./Home.module.css";
+import styles from "./Dashboard.module.css";
 import Header from "../../components/Header/Header";
-import Hero from "../../components/Hero/Hero";
 import Account from "../../components/Account/Account";
 import Footer from "../../components/Footer/Footer";
+import UserDecks from "../../components/UserDecks/UserDecks";
 
 const Dashboard = () => {
   return (
     <div className={styles.wrap}>
       <Header />
-      <Hero />
-      <Account />
+      <div className={styles.dashboard}>
+        <Account />
+        <UserDecks />
+      </div>
       <Footer />
     </div>
   );
