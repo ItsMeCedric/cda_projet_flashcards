c1639e121218459dd31b10cfde5a81e7ec2f1bcd
Cédric <189109074+ItsMeCedric@users.noreply.github.com>
2025-03-06 14:29:34 +0100

    fix index's reducers

--- frontend/src/store/reducers/index.ts
+++ frontend/src/store/reducers/index.ts
@@ -1,11 +1,9 @@
 import authReducer from "./authReducer";
 import searchReducer from "./searchSlice";
-import deckReducer from "./deckReducer";
 
 const reducer = {
   auth: authReducer,
   search: searchReducer,
-  deck: deckReducer,
 };
 
 export default reducer;
