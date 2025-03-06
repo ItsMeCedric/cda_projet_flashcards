d4858fd4563bcc6d81347c63150d57263ae6036b
Cédric <189109074+ItsMeCedric@users.noreply.github.com>
2025-03-06 14:25:16 +0100

    add user's deck on dashboard

--- frontend/src/store/reducers/index.ts
+++ frontend/src/store/reducers/index.ts
@@ -1,9 +1,11 @@
 import authReducer from "./authReducer";
 import searchReducer from "./searchSlice";
+import deckReducer from "./deckReducer";
 
 const reducer = {
   auth: authReducer,
   search: searchReducer,
+  deck: deckReducer,
 };
 
 export default reducer;
