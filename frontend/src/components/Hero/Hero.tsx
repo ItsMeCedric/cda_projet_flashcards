89fe951f0c694eb7336daeaba9799d3b1ea09b7f
Nicolas Vanlauwe <nicolas/vanlauwe@hotmail.com>
2025-03-06 11:24:58 +0100

    ajout du filtre

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,11 +1,15 @@
 import styles from "./Hero.module.css";
+import { useAppSelector } from "../../hooks/redux";
+
 import { useDispatch } from "react-redux";
-import { setSearchContent } from "../../store/reducers/searchSlice";
+import { setSearchContent, toggleTheme, resetThemes } from "../../store/reducers/searchSlice";
 import { useState } from "react";
 
 const Hero = () => {
   const [search, setSearch] = useState("");
+  const themes = ["Math", "Info", "Anglais"];
   const dispatch = useDispatch();
+  const selectedThemes = useAppSelector((state) => state.search.selectedThemes);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     dispatch(setSearchContent(search));
@@ -26,6 +30,22 @@ const Hero = () => {
           placeholder="Search here for deck"
           onChange={(e) => setSearch(e.target.value)}
         />
+        <div className={styles.filter}>
+          <label className={styles.hero_label} htmlFor="filter">
+            Filtres
+          </label>
+          {themes.map((theme) => (
+            <div className={styles.checkbox}>
+              <input
+                type="checkbox"
+                checked={selectedThemes.includes(theme)}
+                onChange={() => dispatch(toggleTheme(theme))}
+              />
+              <label key={theme}>{theme}</label>
+            </div>
+          ))}
+        </div>
+
         <input type="submit" className={styles.search_submit} />
       </form>
     </div>
