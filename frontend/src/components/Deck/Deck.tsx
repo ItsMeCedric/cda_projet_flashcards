17df45a9299dc7e1dce181d486b4b646215ba371
Cédric <189109074+ItsMeCedric@users.noreply.github.com>
2025-03-11 22:37:05 +0100

    fix css issues

--- frontend/src/components/Deck/Deck.tsx
+++ frontend/src/components/Deck/Deck.tsx
@@ -2,6 +2,7 @@ import styles from "./Deck.module.css";
 import { Deck as DeckType } from "../../@types/deck";
 import { useNavigate } from "react-router-dom";
 import { MouseEvent } from "react";
+import { FaPlay, FaAlignCenter } from "react-icons/fa";
 
 const Deck = ({ deck }: { deck: DeckType }) => {
   const navigate = useNavigate();
@@ -22,15 +23,19 @@ const Deck = ({ deck }: { deck: DeckType }) => {
         <path d="M48 24C48 10.7 37.3 0 24 0S0 10.7 0 24L0 64 0 350.5 0 400l0 88c0 13.3 10.7 24 24 24s24-10.7 24-24l0-100 80.3-20.1c41.1-10.3 84.6-5.5 122.5 13.4c44.2 22.1 95.5 24.8 141.7 7.4l34.7-13c12.5-4.7 20.8-16.6 20.8-30l0-279.7c0-23-24.2-38-44.8-27.7l-9.6 4.8c-46.3 23.2-100.8 23.2-147.1 0c-35.1-17.6-75.4-22-113.5-12.5L48 52l0-28zm0 77.5l96.6-24.2c27-6.7 55.5-3.6 80.4 8.8c54.9 27.4 118.7 29.7 175 6.8l0 241.8-24.4 9.1c-33.7 12.6-71.2 10.7-103.4-5.4c-48.2-24.1-103.3-30.1-155.6-17.1L48 338.5l0-237z" />
       </svg>
       <h3>{deck.name}</h3>
-      <img src={deck.url} alt="vignette" />
+      <img className={styles.thumbnail} src={deck.url} alt="vignette" />
       <div className={styles.infos}>
         <p className={styles.description}>{deck.subject}</p>
-        <div>
+        <div className={styles.buttons}>
           {
             //TODO: flex + center
           }
-          <button onClick={openDeckDetails}>Détails</button>
-          <button onClick={playDeck}>Jouer</button>
+          <button className={styles.button} onClick={openDeckDetails}>
+            <FaAlignCenter /> Détails
+          </button>
+          <button className={styles.button} onClick={playDeck}>
+            Jouer <FaPlay />
+          </button>
         </div>
         <div className={styles.icons}>
           <span className={styles.playCount}>
