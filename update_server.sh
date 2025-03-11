1c09f31b9e0f1fa65c96879d85def9ecf618e98d
Laurent <laurent.haar@ccicampus.fr>
2025-03-11 18:20:52 +0000

    fix script

--- update_server.sh
+++ update_server.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-source /home/flashmccards/.nvm/nvm.sh
-sudo -u flashmccards npm --prefix ./frontend run build
+sudo -u flashmccards bash -c 'source /home/flashmccards/.nvm/nvm.sh && npm --prefix ./frontend run build'
 sudo cp -r ./frontend/dist/ /var/www/html
 sudo systemctl restart flashmccards
