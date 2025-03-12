0364e32e8c5da1f3bb1f2adc92c7711c55d272c5
Laurent <seres@seres.eu.org>
2025-03-12 11:03:13 +0100

    removed imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
-import { Routes, Route, useNavigate } from "react-router";
-
+import { Routes, Route } from "react-router";
 import SignIn from "./pages/SignIn/SignIn";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
