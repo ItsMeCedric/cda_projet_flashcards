de3cbd687f8368e102262b893d428c76bd698344
Laurent <seres@seres.eu.org>
2025-03-12 11:53:44 +0100

    fix crash

--- frontend/src/components/AuthRoute.tsx
+++ frontend/src/components/AuthRoute.tsx
@@ -1,13 +1,15 @@
-import { Outlet, useNavigate } from "react-router";
-import { useAppDispatch } from "../hooks/redux";
+import { useAppDispatch, useAppSelector } from "../hooks/redux";
 import { useEffect } from "react";
 import { validateToken } from "../store/actions/authActions";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const AuthRoute = () => {
+  const { user } = useAppSelector((state) => state.auth);
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
   useEffect(() => {
+    if (user === undefined) navigate("/", { replace: true });
     dispatch(validateToken()).then(
       () => {},
       () => {
