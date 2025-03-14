53009e6e36a64121e240a052d48490d56752d75f
Nicolas Vanlauwe <nicolas.vanlauwe@hotmail.com>
2025-03-14 13:49:04 +0100

    click only on image

--- frontend/src/components/Account/Account.tsx
+++ frontend/src/components/Account/Account.tsx
@@ -112,9 +112,14 @@ const Account: React.FC = () => {
           viewBox="0 0 448 512">
           <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
         </svg>
-        <div className={styles.imageContainer} onClick={() => document.getElementById("fileInput")?.click()}>
-          <img className={styles.round} src={profilePicture} alt="user" />
-          <input type="file" id="fileInput" style={{ display: "none" }} accept="image/*" onChange={handleImageChange} />
+        <div className={styles.image_container}>
+          <img
+            className={styles.round}
+            src={profilePicture}
+            alt="user"
+            onClick={() => document.getElementById("fileInput")?.click()}
+          />
+          <input type="file" style={{ display: "none" }} id="fileInput" accept="image/*" onChange={handleImageChange} />
         </div>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.field}>
