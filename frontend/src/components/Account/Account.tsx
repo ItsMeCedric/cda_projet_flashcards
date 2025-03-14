d6dc79247563da58f0c3be4a24714dc4bf540b96
Laurent <seres@seres.eu.org>
2025-03-14 12:31:35 +0100

    bugfix

--- frontend/src/components/Account/Account.tsx
+++ frontend/src/components/Account/Account.tsx
@@ -15,7 +15,7 @@ const Account: React.FC = () => {
   const { user } = useAppSelector((state) => state.auth);
   const { email, username, password, profilePicture } = useAppSelector((state) => state.account.dataAccount);
   // si store pas initialisé alors on prend les valeurs de user
-  if (email == undefined) {
+  if (!email || email == undefined) {
     dispatch(setDataAccount(user));
   }
   const {
