d810e0ef83e718b8d030461e8d83007507668643
Laurent <laurent.haar@ccicampus.fr>
2025-03-17 14:29:37 +0000

    updated update_server.sh

--- update_server.sh
+++ update_server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-sudo -u flashmccards bash -c 'source /home/flashmccards/.nvm/nvm.sh && npm --prefix ./frontend run build'
+sudo -u flashmccards bash -c 'source /home/flashmccards/.nvm/nvm.sh && NODE_ENV=production npm --prefix ./frontend run build'
 sudo cp -r ./frontend/dist/* /var/www/html
+cd backend && NODE_ENV=production npm --prefix ./backend run migrate && NODE_ENV=production npm --prefix ./backend run seed
 sudo systemctl restart flashmccards
