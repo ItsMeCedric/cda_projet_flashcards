0bf19ccefea6345ae9cbd6ba89f5895ff667b146
Laurent <seres@seres.eu.org>
2025-03-18 14:25:34 +0100

    forgot x perms on script

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 echo "Running database migrations!"
-npx sequelize db:migrate
+npm migrate
 
 echo "Running database seeds!"
-npx sequelize db:seed:all
+npm seed
 
 echo "Server starting..."
-npx tsx src/index.ts
+npm start
