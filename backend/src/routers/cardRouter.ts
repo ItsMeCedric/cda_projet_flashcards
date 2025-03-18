24c0d43ae93ae42461bf78b0beced0794766a4dc
Cedric <cedric-beck@ccicampus.fr>
2025-03-18 10:16:49 +0100

    api update

--- backend/src/routers/cardRouter.ts
+++ backend/src/routers/cardRouter.ts
@@ -4,10 +4,131 @@ import verifyAuth from "../middlewares/authMiddleware";
 
 const router = Router({ mergeParams: true });
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Cards
+ *     description: API pour la gestion des cartes
+ */
+
+/**
+ * @swagger
+ * /cards:
+ *   get:
+ *     tags:
+ *       - Cards
+ *     summary: Retourne toutes les cartes d'un jeu
+ *     description: Cette route permet de récupérer toutes les cartes d'un jeu spécifique par son ID.
+ *     parameters:
+ *       - in: path
+ *         name: deckId
+ *         required: true
+ *         description: ID du jeu dont les cartes doivent être récupérées.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Liste des cartes retournée avec succès
+ */
 router.get("/", verifyAuth, cardController.getAllCardsByDeckId);
+
+/**
+ * @swagger
+ * /cards/{cardId}:
+ *   get:
+ *     tags:
+ *       - Cards
+ *     summary: Retourne une carte par son ID
+ *     description: Cette route permet de récupérer une carte spécifique en fonction de son ID.
+ *     parameters:
+ *       - in: path
+ *         name: cardId
+ *         required: true
+ *         description: ID de la carte à récupérer.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Carte retournée avec succès
+ */
 router.get("/:cardId", verifyAuth, cardController.findById);
+
+/**
+ * @swagger
+ * /cards:
+ *   post:
+ *     tags:
+ *       - Cards
+ *     summary: Crée une nouvelle carte
+ *     description: Cette route permet de créer une carte dans un jeu existant.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               front:
+ *                 type: string
+ *               back:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Carte créée avec succès
+ */
 router.post("/", verifyAuth, cardController.create);
+
+/**
+ * @swagger
+ * /cards/{cardId}:
+ *   patch:
+ *     tags:
+ *       - Cards
+ *     summary: Met à jour une carte
+ *     description: Cette route permet de mettre à jour une carte en fonction de son ID.
+ *     parameters:
+ *       - in: path
+ *         name: cardId
+ *         required: true
+ *         description: ID de la carte à mettre à jour.
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               front:
+ *                 type: string
+ *               back:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Carte mise à jour avec succès
+ */
 router.patch("/:cardId", verifyAuth, cardController.update);
+
+/**
+ * @swagger
+ * /cards/{cardId}:
+ *   delete:
+ *     tags:
+ *       - Cards
+ *     summary: Supprime une carte
+ *     description: Cette route permet de supprimer une carte en fonction de son ID.
+ *     parameters:
+ *       - in: path
+ *         name: cardId
+ *         required: true
+ *         description: ID de la carte à supprimer.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Carte supprimée avec succès
+ */
 router.delete("/:cardId", verifyAuth, cardController.destroy);
 
 export default router;
--- backend/src/routers/deckRouter.ts
+++ backend/src/routers/deckRouter.ts
@@ -4,11 +4,152 @@ import verifyAuth from "../middlewares/authMiddleware";
 
 const router = Router({ mergeParams: true });
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Decks
+ *     description: API pour la gestion des decks
+ */
+
+/**
+ * @swagger
+ * /decks:
+ *   get:
+ *     tags:
+ *       - Decks
+ *     summary: Retourne tous les jeux d'un utilisateur
+ *     description: Cette route permet de récupérer tous les jeux associés à un utilisateur donné par son ID.
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         description: ID de l'utilisateur pour lequel récupérer les jeux.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Liste des jeux retournée avec succès
+ */
 router.get("/", verifyAuth, deckController.getAllDecksByUserId);
+
+/**
+ * @swagger
+ * /decks/{deckId}:
+ *   get:
+ *     tags:
+ *       - Decks
+ *     summary: Retourne un jeu par son ID
+ *     description: Cette route permet de récupérer un jeu spécifique en fonction de son ID.
+ *     parameters:
+ *       - in: path
+ *         name: deckId
+ *         required: true
+ *         description: ID du jeu à récupérer.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Jeu retourné avec succès
+ */
 router.get("/:deckId", verifyAuth, deckController.findById);
+
+/**
+ * @swagger
+ * /decks/{deckId}/publish:
+ *   get:
+ *     tags:
+ *       - Decks
+ *     summary: Publie un jeu
+ *     description: Cette route permet de publier un jeu, le rendant accessible publiquement.
+ *     parameters:
+ *       - in: path
+ *         name: deckId
+ *         required: true
+ *         description: ID du jeu à publier.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Jeu publié avec succès
+ */
 router.get("/:deckId/publish", verifyAuth, deckController.publish);
+
+/**
+ * @swagger
+ * /decks:
+ *   post:
+ *     tags:
+ *       - Decks
+ *     summary: Crée un nouveau jeu
+ *     description: Cette route permet de créer un jeu pour l'utilisateur spécifié.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Jeu créé avec succès
+ */
 router.post("/", verifyAuth, deckController.create);
+
+/**
+ * @swagger
+ * /decks/{deckId}:
+ *   patch:
+ *     tags:
+ *       - Decks
+ *     summary: Met à jour un jeu
+ *     description: Cette route permet de mettre à jour un jeu existant en fonction de son ID.
+ *     parameters:
+ *       - in: path
+ *         name: deckId
+ *         required: true
+ *         description: ID du jeu à mettre à jour.
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Jeu mis à jour avec succès
+ */
 router.patch("/:deckId", verifyAuth, deckController.update);
+
+/**
+ * @swagger
+ * /decks/{deckId}:
+ *   delete:
+ *     tags:
+ *       - Decks
+ *     summary: Supprime un jeu
+ *     description: Cette route permet de supprimer un jeu en fonction de son ID.
+ *     parameters:
+ *       - in: path
+ *         name: deckId
+ *         required: true
+ *         description: ID du jeu à supprimer.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Jeu supprimé avec succès
+ */
 router.delete("/:deckId", verifyAuth, deckController.destroy);
 
 export default router;
