d1bf49f72e0e2bf02269de5723d645194d15bd33
cedric <cedric.beck@ccicampus.fr>
2025-03-21 11:20:12 +0100

    fix seeders deleted

---
+++ backend/seeders/20250319094924-deck-themes.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "DeckThemes",
+      [
+        {
+          deckId: 1,
+          themeId: 3,
+        },
+        {
+          deckId: 2,
+          themeId: 1,
+        },
+        {
+          deckId: 3,
+          themeId: 2,
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
