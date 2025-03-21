d92622dee7ebf0edc4f41f8cdfd47e8f8c7c6b21
Laurent <seres@seres.eu.org>
2025-03-21 09:18:55 +0100

    add rgpd button

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -9,18 +9,54 @@ import { useAppSelector } from "../../hooks/redux";
 import { useNavigate } from "react-router-dom";
 import { FaUserSlash } from "react-icons/fa";
 import { FaSheetPlastic } from "react-icons/fa6";
+import { MouseEvent, useState } from "react";
+import { Deck } from "../../@types/deck";
+import { User } from "../../@types/user";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const { user } = useAppSelector((state) => state.auth);
+  const [userData, setUser] = useState<User | undefined>(undefined);
+  const [decks, setDecks] = useState<Deck[]>([]);
+  const [cards, setCards] = useState<Card[]>([]);
 
-  const deleteUser = async () => {
+  const deleteUser = async (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
     const result = confirm("Voulez-vous supprimer votre compte ?");
     if (user && result) {
       await axiosInstance.delete(`/users/${user.id}`);
       navigate("/");
     }
   };
+
+  const downloadUserData = () => {
+    const element = document.createElement("a");
+    const file = new Blob([JSON.stringify(userData), JSON.stringify(decks), JSON.stringify(cards)], {
+      type: "application/json",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = "data.json";
+    document.body.appendChild(element);
+    element.click();
+  };
+
+  const getAllUserData = async (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    axiosInstance.get(`/users/${user?.id}`).then((res) => setUser(res.data));
+    axiosInstance.get(`/users/${user?.id}/decks`).then((res) => {
+      setDecks(res.data);
+      res.data.forEach((deck: Deck) => {
+        axiosInstance.get(`/users/${user?.id}/decks/${deck.id}/cards`).then((res) =>
+          setCards((prev) => {
+            prev.push(res.data);
+            return prev;
+          })
+        );
+      });
+    });
+    downloadUserData();
+  };
+
   return (
     <div className={styles.wrap}>
       <Header />
@@ -28,6 +64,9 @@ const Dashboard = () => {
         <NavLink className={styles.btn_option} to={"/new-deck"}>
           Créer un deck <FaSheetPlastic />
         </NavLink>
+        <a onClick={getAllUserData} className={styles.btn_option}>
+          Récupérer toutes mes données
+        </a>
         <a onClick={deleteUser} className={styles.btn_option}>
           Supprimer le profil <FaUserSlash />
         </a>
